3326bff20c3de81ab0cf59d0c7e5fe08acf50451
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-02-23 00:45:58 +0100

    alert when running out of memory

---
+++ alertmemory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# can be run with:
+# setsid alertmemory.sh >/dev/null 2>&1 < /dev/null &
+# or
+# watch alertmemory.sh
+
+free=$(free -mt | grep Total | awk '{print $4}')
+available=$(free -mt | grep Total | awk '{print $3}')
+available_in_percent=`echo "scale=1; $free / $available * 100" | bc`
+message="$available_in_percent% ($free MB) out of $available MB"
+
+if [[ "$free" -le 5  ]]; then
+	notify-send --urgency=critical "WARNING: $message"
+fi
+
+echo $message
