178b0ffb0586c1d41316223ace904824d05660f9
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-02-23 04:00:35 +0100

    fixed variable mess and floating expression issue

--- alertmemory.sh
+++ alertmemory.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # can be run with:
-# setsid alertmemory.sh >/dev/null 2>&1 < /dev/null &
+# setsid watch alertmemory.sh >/dev/null 2>&1 < /dev/null &
 # or
-# watch alertmemory.sh
+# watch alertmemory.sh >/dev/null 2>&1 </dev/null &
 
 free=$(free -mt | grep Total | awk '{print $4}')
 available=$(free -mt | grep Total | awk '{print $2}')
-available_in_percent=`echo "scale=1; $free / $available * 100" | bc`
-message="$available_in_percent% ($free MB) out of $available MB"
+available_in_percent=`echo "scale=1;$free / $available * 100" | bc`
+available_in_percent_rounded=${available_in_percent%.*}
+message="$available_in_percent% ($free MB) out of $available MB RAM left"
 
-if [[ "$free" -le 5  ]]; then
+if [[ "$available_in_percent_rounded" -le 50 ]]; then
 	notify-send --urgency=critical "WARNING: $message"
 fi
 
